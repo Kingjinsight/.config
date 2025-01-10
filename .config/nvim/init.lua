51fe56bc8dd79ff85324174de9dd9ed86835cf47
King-141319 <kuli141319@gmail.com>
2025-01-10 13:36:23 +0000

    Add more basic config for neovim and hyprland

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,10 @@ require("config.lazy")
 require("config.colors")
 require("lazy").setup("plugins")
 
+
+
+
+
 ---Editor behavior---
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
@@ -22,6 +26,10 @@ vim.cmd("set smartindent")
 vim.cmd("set smarttab")
 vim.cmd("set wrap")
 vim.cmd("colorscheme kanagawa")
+
+
+
+
 
 ---Basic Mappings---
 vim.g.mapleader = " "
@@ -43,6 +51,10 @@ require("fzf-lua").setup({
 	previewers = { ... }, -- Previewers options
 })
 
+
+
+
+
 ---Neotree---
 require("neo-tree").setup({
 	filesystem = {
@@ -55,6 +67,10 @@ require("neo-tree").setup({
 })
 
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { silent = true })
+
+
+
+
 
 ---Lualine---
 require("lualine").setup({
@@ -99,19 +115,29 @@ require("lualine").setup({
 	extensions = {},
 })
 
+
+
+
+
 ---LSP-config---
+
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "jdtls", "pyright" },
 	automatic_installation = true,
 })
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
-lspconfig.lua_ls.setup({})
-lspconfig.jdtls.setup({})
-lspconfig.pyright.setup({})
+lspconfig.lua_ls.setup({ capabilities = capabilities })
+lspconfig.jdtls.setup({ capabilities = capabilities })
+lspconfig.pyright.setup({ capabilities = capabilities })
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+
+
+
 
 ---None-ls---
 local null_ls = require("null-ls")
@@ -119,9 +145,108 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.google-java-format,
 		null_ls.builtins.diagnostics.checkstyle,
-		null_ls.builtins.diagnostics.flake8,
 	},
 })
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+
+
+
+
+---Alpha-nvim---
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+local fortune = require("alpha.fortune")
+
+-- Set header
+dashboard.section.header.val = {
+	"                                                                              ",
+	"████████╗██╗  ██╗███████╗██╗  ██╗ ██████╗    ███╗   ██╗██╗   ██╗██╗███╗   ███╗",
+	"╚══██╔══╝██║  ██║██╔════╝██║ ██╔╝██╔════╝    ████╗  ██║██║   ██║██║████╗ ████║",
+	"   ██║   ███████║█████╗  █████╔╝ ██║         ██╔██╗ ██║██║   ██║██║██╔████╔██║",
+	"   ██║   ██╔══██║██╔══╝  ██╔═██╗ ██║         ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+	"   ██║   ██║  ██║███████╗██║  ██╗╚██████╗    ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
+	"   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
+	"                                                                              ",
+}
+
+dashboard.section.header.opts = {
+	hl = "DashboardHeader",
+	position = "center",
+}
+
+vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#ff9033", bg = "NONE" })
+
+-- Set menu
+dashboard.section.buttons.val = {
+	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "  > Find file", ":cd $HOME/Documents/Coding | lua require'fzf-lua'.files()<CR>"),
+	dashboard.button("r", "  > Recent", ":lua require'fzf-lua'.oldfiles()<CR>"),
+	dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | pwd<CR>"),
+	dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+}
+
+-- Set footer
+dashboard.section.footer.val = fortune()
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
+require("luasnip.loaders.from_vscode").lazy_load()
+
+
+
+
+
+---Completions---
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- For luasnip users.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+
+
+
+
+---Debugger---
+local dap = require("dap")
+local dapui = require("dapui")
+dapui.setup()
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
+
+vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+vim.keymap.set("n", "<leader>dc", dap.continue, {})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,34 +1,56 @@
 return {
-    -- Your plugin list here, e.g.,
-    { "rebelot/kanagawa.nvim" },  -- Kanagawa theme plugin
-    { "ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" }, opts = {} },	--Fzf-lua
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-        config = function () 
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },  
-            })
-        end
-    },  --Treesitter
-    { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    },  -- Neotree
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },  --Lualine
-    {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    }, --LSP-config    
-    { "nvimtools/none-ls.nvim" },  -- none-ls
-
+	-- Your plugin list here, e.g.,
+	{ "rebelot/kanagawa.nvim" }, -- Kanagawa theme plugin
+	{ "ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" }, opts = {} }, --Fzf-lua
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	}, --Treesitter
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	}, -- Neotree
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	}, --Lualine
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	}, --LSP-config
+	{ "nvimtools/none-ls.nvim" }, -- none-ls
+	{
+		"goolord/alpha-nvim",
+		"BlakeJC94/alpha-nvim-fortune",
+	}, --alpha-nvim
+	{ "hrsh7th/nvim-cmp" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+		},
+	},
 }
